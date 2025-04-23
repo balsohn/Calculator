74ef3ca9c8f0a59423cb46314e27d80515f1dbb3
geoson <balsohn@naver.com>
2025-04-23 14:51:16 +0900

    정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력

--- src/com/example/calculator/level1/Calculator.java
+++ src/com/example/calculator/level1/Calculator.java
@@ -17,6 +17,39 @@ public class Calculator {
         System.out.print("사칙연산 기호(➕,➖,✖️,➗) 을 입력해주세요: ");
         String operator = sc.nextLine();
 
+        int result = 0;
+        switch (operator.charAt(0)) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    break;
+                }
+                result = num1 + num2;
+                break;
+            default:
+                if (operator.length() == 1) {
+                    System.out.println("올바른 연산자를 입력해주세요.");
+                    break;
+                } else {
+                    System.out.println("연산자 기호는 하나만 입력 가능합니다.");
+                    break;
+                }
+        }
 
+        System.out.println("계산 결과: " + result);
+
+        sc.close();
+        System.out.println("계산기를 종료합니다.");
     }
+
+
 }
