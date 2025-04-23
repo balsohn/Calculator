605b42b32c21f7aaac4589eb0eb20a3b3eca0a61
geoson <86379869+balsohn@users.noreply.github.com>
2025-04-23 15:12:05 +0900

    Update Calculator.java

--- src/com/example/calculator/level1/Calculator.java
+++ src/com/example/calculator/level1/Calculator.java
@@ -33,7 +33,7 @@ public class Calculator {
                     System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                     break;
                 }
-                result = num1 + num2;
+                result = num1 / num2;
                 break;
             default:
                 if (operator.length() == 1) {
