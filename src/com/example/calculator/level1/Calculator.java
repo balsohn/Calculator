4a517f38d811deb0158a6b7f7f08082264bf33b0
geoson <balsohn@naver.com>
2025-04-23 14:38:20 +0900

    입력 방식 수정

--- src/com/example/calculator/level1/Calculator.java
+++ src/com/example/calculator/level1/Calculator.java
@@ -8,11 +8,11 @@ public class Calculator {
 
         // Scanner를 통해서 양의 정수 입력받기
         Scanner sc = new Scanner(System.in);
-        System.out.println("첫 번째 숫자를 입력해주세요: ");
+        System.out.print("첫 번째 숫자를 입력해주세요: ");
         int num1 = sc.nextInt();
-        System.out.println("두 번째 숫자를 입력해주세요: ");
+        System.out.print("두 번째 숫자를 입력해주세요: ");
         int num2 = sc.nextInt();
-        System.out.println("첫 번째 숫자를 입력해주세요: ");
+
 
     }
 }
